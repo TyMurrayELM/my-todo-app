eaa5ace8165d6a1ee633db838efa0a479646d313
Tyler <tymurrayco@gmail.com>
2025-01-26 13:56:04 -0700

    Updated supabase config with redirect URL

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,6 +5,9 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    redirectTo: 'https://my-todo-app-tau-eight.vercel.app'
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+    redirectTo: 'https://my-todo-app-tau-eight.vercel.app/auth/callback'
   }
 })
