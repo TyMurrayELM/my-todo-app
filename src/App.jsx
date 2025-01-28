9e0bbcc80b8cca08193eca420282bd5470223ac0
Tyler <tymurrayco@gmail.com>
2025-01-28 11:18:10 -0700

    Red hover style for delete

--- src/App.jsx
+++ src/App.jsx
@@ -480,7 +480,7 @@ function App() {
           e.stopPropagation();
           deleteTask(task.id, day);
         }}
-        className="text-gray-400 hover:text-white"
+        className="text-gray-400 hover:text-red"
       >
         <X size={16} />
       </button>
