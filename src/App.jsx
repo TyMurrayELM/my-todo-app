ffa30f75b943dfccb95693af1ff4f3fafbfb0510
Tyler <tymurrayco@gmail.com>
2025-03-05 19:53:00 -0700

    Fixing sign-out button

--- src/App.jsx
+++ src/App.jsx
@@ -570,8 +570,8 @@ const handleToggleHideCompleted = () => {
 />
 <button
   onClick={handleLogout}
-  onTouchEnd={handleLogout}
-  className="text-gray-500 hover:text-gray-700 ml-2 p-2"
+  className="text-gray-500 hover:text-gray-700 ml-2 px-3 py-2 z-50 relative"
+  style={{touchAction: 'manipulation'}}
 >
   Sign Out
 </button>
